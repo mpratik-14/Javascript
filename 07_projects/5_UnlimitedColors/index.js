4a0af44dc9583b7cb1b75dacbda28d421d460cf1
Pratik Mhatre <pdm1452@gmail.com>
2024-10-09 22:35:29 +0530

    Created color change project

---
+++ 07_projects/5_UnlimitedColors/index.js
@@ -0,0 +1,32 @@
+//generate a random color
+
+const randomColor = function(){
+    const hex = "0123456789ABCDEF"
+    let color = '#'
+
+    for(i = 0; i < 6; i++){
+        color += hex[Math.floor(Math.random() * 16)]
+    }
+    return color;
+}
+
+let intervalId
+const startChaningColor = function(){
+    if(!intervalId){
+        intervalId = setInterval(changeBgColor,1000);
+    }
+
+    function changeBgColor(){
+        document.body.style.backgroundColor = randomColor();
+    }
+}
+
+const stopChaningColor = function(){
+    clearInterval(intervalId);
+    intervalId = null;
+}
+
+document.querySelector('#start').addEventListener('click', startChaningColor);
+document.querySelector('#stop').addEventListener('click', stopChaningColor);
+
+
